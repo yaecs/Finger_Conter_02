a63ef531d2a180267355a4b400932b3787ab4563
Anton <kondratov247@mail.ru>
2022-04-04 10:42:59 +0700

    Moday Code

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mp_Hads = mp.solutions.hands
+hands = mp_Hads.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+finger_Coord = [(8, 6), (12, 10), (16, 14), (20, 18)]
+thumb_Coord = (4, 2)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Не удалось получить изображение с web-камеры")
+        continue
+    RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = hands.process(RGB_image)
+    if not result.multi_hand_landmarks:
+        continue
+    multiLandMarks = result.multi_hand_landmarks
+    print("Руки", multiLandMarks)
+    cv2.imshow('image', image)
+    cv2.waitKey(15)

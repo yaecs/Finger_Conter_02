12d0d877db45aa995e6bebcdd93b4278cc740c0d
Anton <kondratov247@mail.ru>
2022-04-07 10:56:04 +0700

    thusday code

--- main.py
+++ main.py
@@ -17,18 +17,37 @@ while cap.isOpened():
     if not success:
         print("Не удалось получить изображение с web-камеры")
         continue
+    
+    image = cv2.flip(image, 1)
+
+    RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = hands.process(RGB_image)
+    if result.multi_hand_landmarks:
+        multiLandMarks = result.multi_hand_landmarks
+        for idx, handLms in enumerate(multiLandMarks):
+            lbl = result.multi_handedness[idx].classification[0].label
+            print(lbl)
+
+        upCount = 0 # кол-во "поднятых" пальцев
+        for handLms in multiLandMarks:
+            mpDraw.draw_landmarks(image, handLms, mp_Hands.HAND_CONNECTIONS)
+            handList = [] # Список пальцев
+            for idx, lm in enumerate(handLms.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                handList.append((cx, cy))
+            for coordinate in finger_Coord:
+                if handList[coordinate[0]][1] < handList[coordinate[1]][1]:
+                    upCount += 1
+            if handList[thumb_Coord[0]][0] > handList[thumb_Coord[1]][0]:
+                upCount += 1
+
+            print(upCount)
+
+
 
     cv2.imshow('image', image)
     if cv2.waitKey(1) &  0xFF == 27:  # esc
         break
-
-    RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    result = hands.process(RGB_image)
-    if not result.multi_hand_landmarks:
-        continue
-    multiLandMarks = result.multi_hand_landmarks
-    print("Руки", multiLandMarks)
-    
-   
 
 cap.release()
